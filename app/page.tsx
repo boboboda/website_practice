46b1a517d923d4288dd0a6d01a92324b587bf75e
bobo <bobo@boboui-MacBookPro.local>
2023-12-11 23:59:53 +0900

    mac

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
-import { divider } from "@nextui-org/theme";
+import "@/styles/globals.css";
 import Head from "next/head"
-import Image from "next/image"
+import { title } from "@/components/primitives";
 
 export default function Home() {
 	return(
-
 	<div>
-<Head>
+		{/* <Head>
 			<title>빡코딩 포토폴리오</title>
 			<meta name="description" content="오늘도 빡코딩"></meta>
 			<link rel="icon" href="/favicon.ico"/>
-		</Head>
+		</Head> */}
 
-		<h1> 홈입니다.</h1>
+
+		<h1 className="text-3xl font-bold underline"> 
+		홈입니다. </h1>
+
+		<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
 	</div>
 	
-		
 	);
 
 }
--- app/releaseApp/page.tsx
+++ app/releaseApp/page.tsx
@@ -1,6 +1,6 @@
 import { title } from "@/components/primitives";
 
-export default function PricingPage() {
+export default function ReleaseAppPage() {
 	return (
 		<div>
 			<h1 className={title()}>출시 앱</h1>
