56607729409188db34b5011b33764d548973b3fa
boboboda <kju9038@gmail.com>
2023-12-17 03:23:47 +0900

    insert

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -2,8 +2,35 @@
 import NextLink from "next/link";
 import Animation from "./animation"
 import { title, subtitle } from "@/components/primitives";
+import Typed from 'typed.js';
+import { useState, useEffect, useRef } from "react";
+
 
 export default function Hero() {
+
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+          strings: [
+            "나만의 홈페이지 또는 어플?", 
+          "비전있는 아이템?", 
+          "충족되지 않는 앱 서비스?", 
+          "그 꿈 부영실이 이루어드리겠습니다."], // Strings to display
+          // Speed settings, try diffrent values untill you get good results
+          startDelay: 300,
+          typeSpeed: 100,
+          backSpeed: 100,
+          backDelay: 300,
+          loop: true
+        });
+    
+        // Destropying
+        return () => {
+          typed.destroy();
+        };
+      }, []);
+
     return (
         <>
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col space-y-5 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -13,9 +40,15 @@ export default function Hero() {
                 <h1 className={title({ size:"sm" })}>
                 저는 코딩천재 부영실입니다.&nbsp; 
 				</h1>
-                <p className={title({ size:"xs", weight:"normal"})}>
-                   홈페이지 개설 준비 중
-                </p>
+
+                <h1 className={title({ size:"xs" })}>
+                여러분은 꿈을 꾸십니까? 
+				</h1>
+
+                <h1 className={title({ size:"xs", color:"pink"})}>
+                <span ref={el}></span>
+                </h1>
+            
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                 <Animation />
--- components/primitives.ts
+++ components/primitives.ts
@@ -13,7 +13,8 @@ export const title = tv({
 			foreground: "dark:from-[#FFFFFF] dark:to-[#4B4B4B]",
 		},
 		size: {
-			xs: "text-[0.3rem] lg:text-[1rem] leading-9",
+			xxs: "text-[0.3rem] lg:text-[1rem] leading-9",
+			xs: "text-2xl lg:text-3xl",
 			sm: "text-3xl lg:text-4xl",
 			md: "text-[2.3rem] lg:text-5xl leading-9",
 			lg: "text-4xl lg:text-6xl",
