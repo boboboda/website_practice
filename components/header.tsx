6450eb0ce0c28bcde5edfa98f4fa712444fbe445
bobo <bobo@boboui-MacBookPro.local>
2024-05-16 02:09:16 +0900

    icon add

--- components/header.tsx
+++ components/header.tsx
@@ -27,8 +27,12 @@ export default function Header() {
 
   return (
     <>
+
+    <Head>
     <meta name="google-site-verification" content="m5OSAuutjyid3qZGPul7bLxNpaLK1TLfY_jCeh5TpXM" />
     <meta name="naver-site-verification" content="f71196e4dbe205d177e771af7db3602a74f06a6d" />
+    </Head>
+    
 
 
     <Navbar className="mt-2" maxWidth="xl" position="sticky">
