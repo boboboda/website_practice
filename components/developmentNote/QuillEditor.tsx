63607f9c19eb451a4d9bcf5d0dc566fa0a6abda8
bobo <bobo@boboui-MacBookPro.local>
2024-05-19 22:46:25 +0900

    insert

--- components/developmentNote/QuillEditor.tsx
+++ components/developmentNote/QuillEditor.tsx
@@ -15,41 +15,43 @@ const QuillEditor: React.FC<QuillEditorProps> = ({ value, onChange }) => {
   const editorRef = useRef<HTMLDivElement | null>(null);
 
   const handleImageUpload = () => {
-    const input = document.createElement('input');
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
-    input.click();
+    if (typeof document !== 'undefined') { // Check if `document` is defined
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/*');
+      input.click();
 
-    input.onchange = async () => {
-      const file = input.files ? input.files[0] : null;
-      if (file) {
-        const storageRef = ref(storage, `images/${file.name}`);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+      input.onchange = async () => {
+        const file = input.files ? input.files[0] : null;
+        if (file) {
+          const storageRef = ref(storage, `images/${file.name}`);
+          const uploadTask = uploadBytesResumable(storageRef, file);
 
-        uploadTask.on(
-          'state_changed',
-          snapshot => {
-            // Progress handling (optional)
-          },
-          error => {
-            console.error('Image upload failed:', error);
-          },
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              if (editorRef.current) {
-                const quill = (editorRef.current as any).__quill as Quill;
-                const range = quill.getSelection(true);
-                quill.insertEmbed(range.index, 'image', downloadURL);
-              }
-            });
-          }
-        );
-      }
-    };
+          uploadTask.on(
+            'state_changed',
+            snapshot => {
+              // Progress handling (optional)
+            },
+            error => {
+              console.error('Image upload failed:', error);
+            },
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                if (editorRef.current) {
+                  const quill = (editorRef.current as any).__quill as Quill;
+                  const range = quill.getSelection(true);
+                  quill.insertEmbed(range.index, 'image', downloadURL);
+                }
+              });
+            }
+          );
+        }
+      };
+    }
   };
 
   useEffect(() => {
-    if (editorRef.current && !(editorRef.current as any).__quill) {
+    if (typeof window !== 'undefined' && editorRef.current && !(editorRef.current as any).__quill) { // Check if `window` is defined
       const quill = new Quill(editorRef.current, {
         theme: 'snow',
         modules: {
