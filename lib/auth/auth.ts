5afef0fda86cfec5904f32eed988ef23b153b524
Jun uk Kim <junukkim@Juns-MacBook-Pro.local>
2025-03-31 13:30:08 +0900

    update

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -206,5 +206,5 @@ export const { handlers, signIn, signOut, auth, unstable_update: update } = Next
       return session;
     }
   },
-  secret: process.env.NEXTAUTH_SECRET
+  secret: process.env.AUTH_SECRET
 })
